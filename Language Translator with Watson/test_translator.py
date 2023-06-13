09c73927c3208960871ac8ccf47f509e41e469da
arqam <arqam@gmail.com>
2023-06-13 19:56:52 +0500

    Modified and added choice in translator File

--- Language Translator with Watson/test_translator.py
+++ Language Translator with Watson/test_translator.py
@@ -4,18 +4,18 @@ from translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertEqual(english_to_french('I want to tell you something but I can not'),
-                         'Je veux vous dire quelque chose, mais je ne peux pas')
+        self.assertEqual(english_to_french('I want to tell you something'),
+                         'Je veux te dire quelque chose')
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertNotEqual(english_to_french('I want to tell you something but I can not'), 'Bonjour')
+        self.assertNotEqual(english_to_french('I want to tell you something'), 'Bonjour')
 
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
-        self.assertEqual(french_to_english('Je veux vous dire quelque chose, mais je ne peux pas'),
-                         'I want to tell you something but I can not')
+        self.assertEqual(french_to_english('Je veux te dire quelque chose'),
+                         'I want to tell you something')
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
-        self.assertNotEqual(french_to_english('Je veux vous dire quelque chose, mais je ne peux pas'), 'Hello')
+        self.assertNotEqual(french_to_english('Je veux te dire quelque chose'), 'Hello')
 
 
 unittest.main()
--- Language Translator with Watson/translator.py
+++ Language Translator with Watson/translator.py
@@ -24,4 +24,36 @@ def french_to_english(text1):
 
     return englishtranslation.get("translations")[0].get("translation")
 
-print(english_to_french('Hello'))
+# English to urdu
+def english_to_urdu(text1):
+    """
+    This function will translate from english to urdu
+    """
+    urdutranslation = language_translator.translate(text=text1, model_id='en-ur').get_result()
+    return urdutranslation.get("translations")[0].get("translation")
+
+# French to English
+print(french_to_english('Je veux te dire quelque chose'))
+
+while True:
+    print("For English to Urdu: Press 1\n")
+    print("For English to French: Press 2\n")
+    print("For Exit: Press 3")
+    choice = int(input("Make your choice: "))
+
+    if(choice ==  1):
+        texts = input("Enter sentence in English: ")
+        print(english_to_urdu(texts))
+    elif(choice == 2):
+        texts = input("Enter sentence in English: ")
+        print(english_to_french(texts))
+    elif(choice == 3):
+        print("Thanks for Using our language Translator")
+        break
+    else:
+        print("You have entered a wrong choice: Try Again")
+
+
+
+
+
