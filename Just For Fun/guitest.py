75cefdfa9d143550a5e9da2cbf25d0b3547b2d77
arqam <arqam@gmail.com>
2023-06-13 20:28:16 +0500

    For testing GUI on cloud

---
+++ Just For Fun/guitest.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Create a new tkinter window
+window = tk.Tk()
+
+# Add a label to the window
+label = tk.Label(window, text="Hello, Arqam. This is GUI Display on console based env!")
+label.pack()
+
+# Start the GUI event loop
+window.mainloop()
